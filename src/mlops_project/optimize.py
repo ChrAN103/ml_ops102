af1e7ee6e31b7a206df517b0730b0a0d1b22225f
JuliusGGW <juliuswinkel@hotmail.com>
2026-01-21 15:16:06 +0100

    updates optimization to save vocab

--- src/mlops_project/optimize.py
+++ src/mlops_project/optimize.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import Tuple
+import json
 
 import torch
 from torch import nn
@@ -27,12 +28,14 @@ def load_model(model_path: Path = MODEL_PATH) -> Model:
     vocab_size = checkpoint.get("vocab_size")
     hyper_params = checkpoint.get("hyper_parameters", {})
     hyper_params_clean = {key: value for key, value in hyper_params.items() if key != "vocab_size"}
-    return Model.load_from_checkpoint(
+    model = Model.load_from_checkpoint(
         model_path,
         vocab_size=vocab_size,
         **hyper_params_clean,
         strict=False,
     )
+    vocab = checkpoint.get("vocab")
+    return model, vocab
 
 def _parameters_to_prune(model: nn.Module) -> Tuple[Tuple[nn.Module, str], ...]:
     """Collect prunable parameters across the model.
@@ -77,6 +80,31 @@ def prune_model(model: nn.Module, amount: float = 0.2) -> nn.Module:
         pruning.remove(module, param_name)
     return model
 
+def save_vocab(vocab: dict[str, int], output_path: Path) -> None:
+    """Save vocab in a standard JSON format."""
+    with open(output_path, "w", encoding="utf-8") as f:
+        json.dump(vocab, f, indent=2)
+
+
+def _quantatize_onnx(model_path: Path) -> nn.Module:
+    """Quantize an ONNX model using dynamic quantization."""
+
+    onnx_preprocessed = model_path.with_name(model_path.stem + "_preprocessed.onnx")
+
+    quant_pre_process(
+        input_model=str(model_path),
+        output_model_path=str(onnx_preprocessed),
+        skip_optimization=False
+    )
+                                           
+    quantize_dynamic(
+        model_input=str(onnx_preprocessed),
+        model_output=str(model_path),
+        weight_type=QuantType.QInt8
+    )
+
+    onnx_preprocessed.unlink()
+
 
 def onnx_port(
     model: nn.Module,
@@ -98,37 +126,24 @@ def onnx_port(
     """
 
     model.eval()
-    onnx_raw = output_path.with_name(output_path.stem + "_raw.onnx")
-    onnx_preprocessed = output_path.with_name(output_path.stem + "_preprocessed.onnx")
 
     dummy_input = torch.zeros((batch_size, seq_length), dtype=torch.long)
 
     torch.onnx.export(
         model=model,
         args=(dummy_input,),
-        f=str(onnx_raw),
+        f=str(output_path),
         input_names=["input"], 
         output_names=["output"],
         dynamic_axes={"input": {0: "batch_size", 1: "seq_len"}, "output": {0: "batch_size"}},
         dynamo=False,
     )
 
-    quant_pre_process(
-            input_model=str(onnx_raw),
-            output_model_path=str(onnx_preprocessed),
-            skip_optimization=False
-        )
-                                           
-    quantize_dynamic(
-        model_input=str(onnx_preprocessed),
-        model_output=str(output_path),
-        weight_type=QuantType.QInt8
-    )
+    _quantatize_onnx(output_path)
 
-    onnx_preprocessed.unlink()
-    onnx_raw.unlink()
 
 if __name__ == "__main__":
-    model = load_model()
+    model, vocab = load_model()
+    save_vocab(vocab, output_path=Path("models/vocab.json"))
     # pruned_model = prune_model(model, amount=0.3)
     onnx_port(model, output_path=Path("models/model_optimized.onnx"))
